58acf4840211f6ebc3cd57063b4c846d903ed906
Md Azizul Hakim <ahslucky12@gmail.com>
2020-08-15 21:59:13 +0600

    initial commit
    This commit contains basic features of yajra datatables
    
    customizaing row and columns,columns value manupulation,render changes,adding calumn manually

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Yajra\Datatables\Datatables;
+
+class HomeController extends Controller
+{
+    //
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function users(Request $request)
+    {
+        //$collection = [];
+        $users =  User::all('id', 'name', 'email', 'status');
+        // dd($users);
+        // foreach ($users as $user) {
+        //     if ($user->status == 1) {
+        //         $userData = [
+        //             "id" => $user->id,
+        //             "name" => $user->name,
+        //             "email" => $user->email,
+        //             'status' => "active"
+
+        //         ];
+        //     } else {
+        //         $userData = [
+        //             "id" => $user->id,
+        //             "name" => $user->name,
+        //             "email" => $user->email,
+        //             'status' => "Deactive"
+
+        //         ];
+        //     }
+
+        //     array_push($collection, $userData);
+        // }
+        //dd($collection);
+
+        //the basic
+       // return Datatables::of($users)->make(true);
+
+        //row customization
+
+        return Datatables::of($users)
+        ->setRowClass(function ($user) {
+            return $user->status == 1 ?  : 'alert-warning';
+        })
+        ->editColumn('status', function ($user) {
+            if($user->status == 1){
+                $markup = '<span class="text-success">Active</span>';
+            } else{
+                $markup = '<span class="text-warning">Deactive</span>';
+            }
+            
+            return $markup;
+        })
+        ->addColumn('action', function($user){
+            $button = '<button type="button" name="edit" data-id="'.$user->id.'" onclick="editUser('.$user->id.')" class="edit btn btn-primary btn-sm">Edit</button>';
+            $button .= '&nbsp;&nbsp;';
+            $button .= '<button type="button" name="delete" data-id="'.$user->id.'"onclick="deleteUser('.$user->id.')" class="delete btn btn-danger btn-sm">Delete</button>';
+            return $button;
+        })
+        ->rawColumns(['status','action'])
+        ->make(true);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="{{asset('bootstrap/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('datatable/dataTables.bootstrap4.min.css')}}">
+
+    <script src="{{asset('pace/pace.min.js')}}"></script>
+    <link href="{{asset('pace/themes/red/pace-theme-minimal.css')}}" rel="stylesheet" />
+    <title>Laravel-Server Side Datatable</title>
+</head>
+
+<body>
+    <div class="container" id="">
+        {{-- <h1>Hello, world!</h1> --}}
+        <div>
+
+        </div>
+        <h1 class="font-weight-light text-center text-lg-left mt-4 mb-2">Datatable with 5000 data SERVER SIDE Processing
+        </h1>
+
+        <hr class="mt-2 mb-5">
+
+        <div class="row" id="data-table">
+            <div class="table-responsive">
+                <table id="example" class="table table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tfoot>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script> --}}
+    <script src="{{asset('bootstrap/jquery-3.2.1.min.js')}}"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script> --}}
+    <script src="{{asset('bootstrap/popper.min.js')}}"></script>
+    {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script> --}}
+
+    <script src="{{asset('bootstrap/bootstrap.min.js')}}"></script>
+
+    <script src="{{asset('datatable/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('datatable/dataTables.bootstrap4.min.js')}}"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#example').dataTable({
+                "processing": true,
+                "serverSide": true,
+                "ajax": "{{ url('/users') }}",
+                columns: [{
+                        data: 'id',
+                        name: 'id'
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'email',
+                        name: 'email'
+                    },
+                    {
+                        data: 'status',
+                        name: 'status'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false
+                    }
+                ]
+            });
+        });
+
+        function editUser(id) {
+            alert(id);
+        };
+
+        function deleteUser(id) {
+            alert(id);
+        };
+
+    </script>
+
+</body>
+
+</html>
